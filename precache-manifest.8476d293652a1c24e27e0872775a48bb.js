840f3af4ada45ba98b5456659f5124f96d7d6d93
warbonnet333 <warbonnet333@gmail.com>
2020-06-22 22:02:40 +0300

    Updates

---
+++ precache-manifest.8476d293652a1c24e27e0872775a48bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734714d90bd61a6418a77c4a72ed7eca",
+    "url": "/oxota_na_ovets/index.html"
+  },
+  {
+    "revision": "8c829bf7070bc69cfbdb",
+    "url": "/oxota_na_ovets/static/css/main.2d47825f.chunk.css"
+  },
+  {
+    "revision": "99c7753a843bfa5836ee",
+    "url": "/oxota_na_ovets/static/js/2.8accae5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oxota_na_ovets/static/js/2.8accae5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c829bf7070bc69cfbdb",
+    "url": "/oxota_na_ovets/static/js/main.cb6c301f.chunk.js"
+  },
+  {
+    "revision": "fad6983df4614e531533",
+    "url": "/oxota_na_ovets/static/js/runtime-main.0d89ca5a.js"
+  },
+  {
+    "revision": "90676870ae0da4d6c89baa0712f369c0",
+    "url": "/oxota_na_ovets/static/media/collage.90676870.jpg"
+  },
+  {
+    "revision": "0d260e0d5c0afe613ad2b0253f694917",
+    "url": "/oxota_na_ovets/static/media/iconfinder_arrow-circle-up_1608521.0d260e0d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oxota_na_ovets/static/media/logo.5d5d9eef.svg"
+  }
+]);
